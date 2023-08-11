de529b5f14ae860aa494159c5c3cb7bf02d2c546
DRXXX888 <85310345+DRXXX888@users.noreply.github.com>
2023-08-11 12:40:22 +0700

    Update Uitedt.lua

--- Uitedt.lua
+++ Uitedt.lua
@@ -6165,29 +6165,3 @@ function library:CreateWindow(options, ...)
 	end
 	return windowFunctions
 end
-
-local ScreenGui1 = Instance.new("ScreenGui")
-local ImageButton1 = Instance.new("ImageButton")
-local UICorner = Instance.new("UICorner")
-
-ScreenGui1.Name = "ImageButton"
-ScreenGui1.Parent = game.CoreGui
-ScreenGui1.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-ImageButton1.Parent = ScreenGui1
-ImageButton1.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-ImageButton1.BorderSizePixel = 0
-ImageButton1.Position = UDim2.new(0.120833337, 0, 0.0952890813, 0)
-ImageButton1.Size = UDim2.new(0, 50, 0, 50)
-ImageButton1.Draggable = true
-ImageButton1.Image = "http://www.roblox.com/asset/?id=13186299760"
-ImageButton1.MouseButton1Down:connect(function()
-  game:GetService("VirtualInputManager"):SendKeyEvent(true,305,false,game)
-  game:GetService("VirtualInputManager"):SendKeyEvent(false,305,false,game)
-end)
-UICorner.Parent = ImageButton1
-
-library.NewWindow = library.CreateWindow
-library.AddWindow = library.CreateWindow
-library.Window = library.CreateWindow
-library.W = library.CreateWindow
